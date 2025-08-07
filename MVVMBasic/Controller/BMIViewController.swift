ef3fecfaab2b2a3e231666fa416c838697ebf3db
moklad <dev.moklad@gmail.com>
2025-08-08 02:04:36 +0900

    feat: BMI 계산기 버튼 눌렸을 떄 처리할 로직 구현
    - Typed Throws를 사용하여 에러 처리 간소화
    - 몸무게, 키 검증 함수 구현
    - BMI 계산 로직 구현

--- MVVMBasic/Controller/BMIViewController.swift
+++ MVVMBasic/Controller/BMIViewController.swift
@@ -14,7 +14,7 @@ final class BMIViewController: UIViewController {
         textField.borderStyle = .roundedRect
         return textField
     }()
-    private let ageTextField: UITextField = {
+    private let weightTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "몸무게를 입력해주세요"
         textField.borderStyle = .roundedRect
@@ -48,14 +48,22 @@ final class BMIViewController: UIViewController {
     
     @objc private func resultButtonTapped() {
         view.endEditing(true)
+        do {
+            let values = try validate(height: heightTextField.text, weight: weightTextField.text)
+            let bmi = calculateBMI(height: values.height, weight: values.weight)
+            let result = getResult(from: bmi)
+            
+            resultLabel.text = result
+        } catch {
+            showAlert(title: "오류", message: error.description)
+        }
     }
 }
-
 
 extension BMIViewController {
     func configureHierarchy() {
         view.addSubview(heightTextField)
-        view.addSubview(ageTextField)
+        view.addSubview(weightTextField)
         view.addSubview(resultButton)
         view.addSubview(resultLabel)
     }
@@ -67,14 +75,14 @@ extension BMIViewController {
             make.height.equalTo(44)
         }
         
-        ageTextField.snp.makeConstraints { make in
+        weightTextField.snp.makeConstraints { make in
             make.top.equalTo(heightTextField.snp.bottom).offset(20)
             make.horizontalEdges.equalToSuperview().inset(20)
             make.height.equalTo(44)
         }
         
         resultButton.snp.makeConstraints { make in
-            make.top.equalTo(ageTextField.snp.bottom).offset(20)
+            make.top.equalTo(weightTextField.snp.bottom).offset(20)
             make.horizontalEdges.equalToSuperview().inset(20)
             make.height.equalTo(44)
         }
@@ -90,5 +98,54 @@ extension BMIViewController {
         configureBorder(target: resultButton, radius: 8)
         configureBackgroundColor(from: resultButton, color: .systemBlue)
         configureAlignment(for: &resultLabel, to: .center)
+    }
+}
+
+extension BMIViewController {
+    private func validate(height: String?, weight: String?) throws(BMIValidationError) -> (height: Double, weight: Double) {
+        guard let text = height, !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            throw BMIValidationError.isEmpty(value: "키")
+        }
+        guard let height = Double(text) else {
+            throw BMIValidationError.notANumber(value: "키")
+        }
+        
+        guard (65...289).contains(height) else {
+            throw BMIValidationError.notValidInput(value: "키")
+        }
+        
+        guard let text = weight, !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            throw BMIValidationError.isEmpty(value: "몸무게")
+        }
+        
+        guard let weight = Double(text) else {
+            throw BMIValidationError.notANumber(value: "몸무게")
+        }
+        
+        guard (1...635).contains(weight) else {
+            throw BMIValidationError.notValidInput(value: "몸무게")
+        }
+        
+        return (height, weight)
+    }
+    
+    private func calculateBMI(height: Double, weight: Double) -> Double {
+        let height = height / 100
+        let bmi = weight / (height * height)
+        return bmi
+    }
+    
+    private func getResult(from bmi: Double) -> String {
+        let result = String(format: "%.1f", bmi)
+        switch bmi {
+        case ..<18.5:
+            return ("당신의 BMI 지수는, \(result)로 저체중입니다")
+        case 18.5..<23:
+            return ("당신의 BMI 지수는, \(result)로 정상입니다.")
+        case 23..<25:
+            return ("당신의 BMI 지수는, \(result)로과체중입니다.")
+        default:
+            return ("당신의 BMI 지수는, \(result)로 비만입니다.")
+        }
     }
 }

26a028e2108c6f7d4624ffb3f6aa7474569499f0
moklad <dev.moklad@gmail.com>
2025-08-07 23:12:24 +0900

    feat: UILabel 과 UITextField에 TextAlignmentConfiguratble 프로토콜 채택

---
+++ MVVMBasic/Extensions/UILabel+Extnesion.swift
@@ -0,0 +1,12 @@
+//
+//  UILabel+Extnesion.swift
+//  MVVMBasic
+//
+//  Created by Youngjun Kim on 8/7/25.
+//
+
+import UIKit
+
+extension UILabel: TextAlignmentConfiguable {
+    
+}
---
+++ MVVMBasic/Extensions/UITextField+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UITextField+Extension.swift
+//  MVVMBasic
+//
+//  Created by Youngjun Kim on 8/7/25.
+//
+
+import UIKit
+
+extension UITextField: TextAlignmentConfiguable {
+    
+}

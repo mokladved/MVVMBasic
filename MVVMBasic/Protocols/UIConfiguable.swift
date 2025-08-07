5464a845b5d32331b366fd2254c88abb798e342b
moklad <dev.moklad@gmail.com>
2025-08-07 21:56:42 +0900

    feat: UI 구조화를 위한 프로토콜 정의

---
+++ MVVMBasic/Protocols/UIConfiguable.swift
@@ -0,0 +1,14 @@
+//
+//  UIConfiguable.swift
+//  MVVMBasic
+//
+//  Created by Youngjun Kim on 8/7/25.
+//
+
+import Foundation
+
+protocol UIConfiguable {
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}

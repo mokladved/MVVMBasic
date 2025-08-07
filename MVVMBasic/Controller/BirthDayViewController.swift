1db5198714ce3af4dc2a4a4eadef58da16b9ac12
moklad <dev.moklad@gmail.com>
2025-08-08 03:47:31 +0900

    feat: 생년월일 버튼 탭할 시 처리 로직 구현
    - typed throws를 활용한 에러 처리 코드 간소화
    - DateFormatter 의 isLenient 프로퍼티를 통해 월, 일이 유효한 값인지 확인
    - String(format:)을 통해 월, 일에 한자리 숫자를 입력해도 앞에 0이 붙도록하여 날짜 포맷에 맞도록 구현

--- MVVMBasic/Controller/BirthDayViewController.swift
+++ MVVMBasic/Controller/BirthDayViewController.swift
@@ -68,9 +68,18 @@ final class BirthDayViewController: UIViewController {
     
     @objc private func resultButtonTapped() {
         view.endEditing(true)
+        
+        do {
+            let birthDate = try validate(year: yearTextField.text, month: monthTextField.text, day: dayTextField.text)
+            if let dDay = calculateDDays(from: birthDate) {
+                resultLabel.text = "오늘 날짜를 기준으로 D+\(dDay)입니다."
+            } else {
+                showAlert(title: "계산 오류", message: "날짜 계산이 유효하지 않습니다.")
+            }
+        } catch {
+            showAlert(title: "오류", message: error.description)
+        }
     }
-    
-
 }
 
 extension BirthDayViewController: UIConfiguable {
@@ -139,5 +148,58 @@ extension BirthDayViewController: UIConfiguable {
         configureBackgroundColor(from: resultButton, color: .systemBlue)
         configureAlignment(for: &resultLabel, to: .center)
         
+    }
+}
+
+// TODO: 다른 유효한 날짜 검증 방법 찾기
+extension BirthDayViewController {
+    private func validate(year: String?, month: String?, day: String?) throws(BirthDayValidationError) -> Date {
+        guard let yearText = year, !yearText.trimmingCharacters(in: .whitespaces).isEmpty else {
+            throw BirthDayValidationError.isEmpty(field: "년")
+        }
+        
+        guard let monthText = month, !monthText.trimmingCharacters(in: .whitespaces).isEmpty else {
+            throw BirthDayValidationError.isEmpty(field: "월")
+        }
+        
+        guard let dayText = day, !dayText.trimmingCharacters(in: .whitespaces).isEmpty else {
+            throw BirthDayValidationError.isEmpty(field: "일") }
+        
+        guard let year = Int(yearText) else {
+            throw BirthDayValidationError.notANumber(field: "년")
+        }
+        
+        guard let month = Int(monthText) else {
+            throw BirthDayValidationError.notANumber(field: "월")
+        }
+        
+        guard let day = Int(dayText) else {
+            throw BirthDayValidationError.notANumber(field: "일")
+        }
+        
+        let formattedMonth = String(format: "%02d", month)
+        let formattedDay = String(format: "%02d", day)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        formatter.isLenient = false
+        
+        let dateString = "\(year)\(formattedMonth)\(formattedDay)"
+        
+        if let date = formatter.date(from: dateString) {
+            return date
+        }
+        throw BirthDayValidationError.notValidDate
+    }
+    
+    private func calculateDDays(from past: Date) -> Int? {
+        let calendar = Calendar.current
+        
+        let now = Date()
+        let today = calendar.startOfDay(for: now)
+        let past = calendar.startOfDay(for: past)
+        
+        let result = calendar.dateComponents([.day], from: past, to: today)
+        return result.day
     }
 }

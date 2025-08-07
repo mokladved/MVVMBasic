f5e3a758182432537a41065592a17bc998dc1ca1
moklad <dev.moklad@gmail.com>
2025-08-08 00:36:45 +0900

    feat: 검증 결과에 대해 Alert을 주기 위해 UIViewController Extension에 메서드 추가

--- MVVMBasic/Extensions/UIViewController+Extension.swift
+++ MVVMBasic/Extensions/UIViewController+Extension.swift
@@ -24,4 +24,11 @@ extension UIViewController {
     func configureBackgroundColor<T: UIView> (from view: T, color: UIColor) {
         view.backgroundColor = color
     }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
 }

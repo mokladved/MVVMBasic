228eaa026dfaf56e10016032965ca19ca38ae328
moklad <dev.moklad@gmail.com>
2025-08-08 02:20:10 +0900

    feat: Error 프로토콜을 채택한 생일 입력 검증용 Enum 생성

---
+++ MVVMBasic/Enums/BirthDayValidationError.swift
@@ -0,0 +1,25 @@
+//
+//  BirthDayValidationError.swift
+//  MVVMBasic
+//
+//  Created by Youngjun Kim on 8/8/25.
+//
+
+import Foundation
+
+enum BirthDayValidationError: Error {
+    case isEmpty(field: String)
+    case notANumber(field: String)
+    case notValidDate
+    
+    var description: String {
+        switch self {
+        case .isEmpty(let value):
+            return "\(value) 입력을 해주세요."
+        case .notANumber(let value):
+            return "\(value)에는 숫자만 입력해주세요."
+        case .notValidDate:
+            return "유효하지 않은 날짜입니다."
+        }
+    }
+}

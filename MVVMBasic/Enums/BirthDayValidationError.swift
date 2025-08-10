8d79623028275f997a40b648724597baa8b1aaed
moklad <dev.moklad@gmail.com>
2025-08-11 02:52:12 +0900

    feat: 에러케이스 추가

--- MVVMBasic/Enums/BirthDayValidationError.swift
+++ MVVMBasic/Enums/BirthDayValidationError.swift
@@ -11,6 +11,7 @@ enum BirthDayValidationError: Error {
     case isEmpty(field: String)
     case notANumber(field: String)
     case notValidDate
+    case failCalculate
     
     var description: String {
         switch self {
@@ -20,6 +21,8 @@ enum BirthDayValidationError: Error {
             return "\(value)에는 숫자만 입력해주세요."
         case .notValidDate:
             return "유효하지 않은 날짜입니다."
+        case .failCalculate:
+            return "날짜 계산이 유효하지 않습니다."
         }
     }
 }

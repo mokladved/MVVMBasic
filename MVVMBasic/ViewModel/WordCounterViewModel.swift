3005402b61273930907fe9b5be0142f53126fe94
moklad <dev.moklad@gmail.com>
2025-08-10 17:20:26 +0900

    refatcor: VC 로직 분리
    - 글자수를 계산하여 label에 표시하는 로직을 VM이 담당하도록 구현
    - 글자가 입력되면 VM으로 전달되고, VM의 updatedText가 호출되어 VC로 데이터 전달

---
+++ MVVMBasic/ViewModel/WordCounterViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  WordCounterVIewModel.swift
+//  MVVMBasic
+//
+//  Created by Youngjun Kim on 8/10/25.
+//
+
+import Foundation
+
+class WordCounterViewModel {
+    var inputField = "" {
+        didSet {
+            countText()
+        }
+    }
+    
+    private var ouputText = "" {
+        didSet {
+            textUpdated?(ouputText)
+        }
+    }
+    
+    var textUpdated: ((String) -> Void)?
+
+    private func countText() {
+        let count = inputField.count
+        ouputText = "현재까지 \(count)글자 작성중"
+    }
+}
